0a1e0bec4c405ef07cd82956fa30aef04ca92367
nejakotnik <nk5058@student.uni-lj.si>
2021-02-27 21:47:31 +0100

    dodajam izracun

--- Gravitacija.java
+++ Gravitacija.java
@@ -6,4 +6,15 @@ public class Gravitacija {
 
         System.out.println("OIS je zakon!");
     }
+
+    public static double izracun(int h){
+        Scanner sc=new Scanner(System.in);
+        double v = sc.nextDouble();
+        double c = 667400;
+        double m = 597200000;
+        double r = 6371000;
+
+        double a=(cm)/((r+v)*(r+v));
+        return a;
+    }
 }
