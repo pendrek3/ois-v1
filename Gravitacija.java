635301033177cfa999ae856e1ee8b238d2eb5fc9
nejc-j <jj6493@student.uni-lj.si>
2021-02-27 22:11:30 +0100

    Dodajanje izpisa pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -5,5 +5,11 @@ public class Gravitacija {
     public static void main(String[] args) {
 
         System.out.println("OIS je zakon!");
+
+    }
+
+    public static void izpisiVrednost(double v,double a)
+    {
+        System.out.printf("Nadmorska višina je: %f\nGravitacijski pospešek je: %f", v, a);
     }
 }
